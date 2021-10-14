42380cda29bcc61c7702c4d6b00482b4e91866d8
madummuhammad <muhammad.madum2018@gmail.com>
2021-10-14 21:32:03 +0700

    Menambahkan home

---
+++ application/views/website/partial/footer.php
@@ -0,0 +1,33 @@
+	<div class="footer-bottom">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="d-sm-flex justify-content-between align-items-center">
+						<div class="fs-14 font-weight-600">
+							© 2020 @ <a href="https://www.bootstrapdash.com/" target="_blank" class="text-white"> BootstrapDash</a>. All rights reserved.
+						</div>
+						<div class="fs-14 font-weight-600">
+							Handcrafted by <a href="https://www.bootstrapdash.com/" target="_blank" class="text-white">BootstrapDash</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<!-- partial -->
+</div>
+</div>
+<!-- inject:js -->
+<script src="<?php echo base_url()?>assets/vendors/js/vendor.bundle.base.js"></script>
+<!-- endinject -->
+<!-- plugin js for this page -->
+<script src="<?php echo base_url()?>assets/vendors/aos/dist/aos.js/aos.js"></script>
+<!-- End plugin js for this page -->
+<!-- Custom js for this page-->
+<script src="<?php echo base_url()?>/assets/js/demo.js"></script>
+<script src="<?php echo base_url()?>/assets/js/jquery.easeScroll.js"></script>
+<!-- End custom js for this page-->
+</body>
+</html>
