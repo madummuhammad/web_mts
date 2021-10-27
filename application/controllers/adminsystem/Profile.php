ecf98c705d77b889d0859aab0bfe75ddfc0e3736
madummuhammad <muhammad.madum2018@gmail.com>
2021-10-27 19:37:50 +0700

    edit profile dan ganti password

--- application/controllers/adminsystem/Profile.php
+++ application/controllers/adminsystem/Profile.php
@@ -13,11 +13,22 @@ class Profile extends CI_Controller {
 		$data['user']=$this->session->userdata();
 		$data['title']='Profile';
 		$data['profile']=$this->M_Auth->tampil_profile();
+		$data['ruang_gedung']=$this->M_Profile->tampil_ruang_gedung();
 		$this->load->view('admin/partial/header');
 		$this->load->view('admin/partial/topbar');
 		$this->load->view('admin/partial/sidebar');
 		$this->load->view('admin/partial/breadcrumb',$data);
 		$this->load->view('admin/v_profile',$data);
 		$this->load->view('admin/partial/footer');
+	}
+
+	public function edit_profile()
+	{
+		$this->M_Auth->edit_profile();
+	}
+
+	public function edit_sandi()
+	{
+		$this->M_Auth->edit_sandi();
 	}
 }
--- application/views/admin/v_profile.php
+++ application/views/admin/v_profile.php
@@ -55,17 +55,24 @@
                                                 <div class="profile-personal-info">
                                                     <h4 class="text-primary mb-4 mt-4 text-center">Informasi Profil</h4>
                                                     <form>
+                                                        <div class="form-group">
+                                                            <img height="300" width="300" src="<?= base_url('assets/images/upload/profil/').$profile['gambar'] ?>" id="preview" class="img-thumbnail">
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="gambar">Foto Profile</label>
+                                                            <input id="gambar-profile" type="file" name="gambar-profile" class="form-control">
+                                                        </div>
                                                         <div class="form-row">
                                                             <div class="form-group col-md-12">
                                                                 <label>Username</label>
-                                                                <input type="text" placeholder="Ganti Username" class="form-control" value="<?= $profile['username'] ?>">
+                                                                <input type="text" placeholder="Ganti Username" name="username" class="form-control" value="<?= $profile['username'] ?>">
                                                             </div>
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Nama</label>
-                                                            <input type="text" placeholder="Ganti Nama" class="form-control" value="<?= $profile['nama'] ?>">
+                                                            <input type="text" placeholder="Ganti Nama" class="form-control" name="nama" value="<?= $profile['nama'] ?>">
                                                         </div>
-                                                        <button class="btn btn-primary" type="submit">Ganti Profile</button>
+                                                        <button class="btn btn-primary" type="button" id="submit-edit-profile">Ganti Profile</button>
                                                     </form>
                                                 </div>
                                             </div>
@@ -76,13 +83,13 @@
                                                         <form>
                                                             <div class="form-group">
                                                                 <label>Password Lama</label>
-                                                                <input type="text" placeholder="Password Lama" class="form-control">
+                                                                <input type="password" placeholder="Password Lama" name="password_lama" class="form-control">
                                                             </div>
                                                             <div class="form-group">
                                                                 <label>Password Baru</label>
-                                                                <input type="text" placeholder="Password Baru" class="form-control">
+                                                                <input type="password" placeholder="Password Baru" name="password_baru" class="form-control">
                                                             </div>
-                                                            <button class="btn btn-primary" type="submit">Ganti Sandi</button>
+                                                            <button class="btn btn-primary" type="button" id="submit-ganti-sandi">Ganti Sandi</button>
                                                         </form>
                                                     </div>
                                                 </div>
